c7a8be06f819e19d877dca1d4e00936d863328a0
Ben <105577567+benjbright@users.noreply.github.com>
2022-10-06 15:59:17 +0100

    Completion of Ethereum section
    Completed update of the course project with the Campaign Factory contract deployed on the Goerli test network

--- ethereum/scripts/deploy.js
+++ ethereum/scripts/deploy.js
@@ -1,31 +1,27 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers } = require("hardhat")
+
+const CampaignFactory = require("../artifacts/contracts/Campaign.sol/CampaignFactory.json")
+const abi = CampaignFactory.abi
+const byteCode = CampaignFactory.bytecode
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const accounts = await ethers.getSigners()
+  const signer = accounts[0]
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  const campaignFactory = await ethers.getContractFactory(abi, byteCode, signer)
+  const contract = await campaignFactory.deploy()
+  console.log("Deploying contract...")
+  await contract.deployed()
+  console.log("Contract deployed...")
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log(`The contract has been deployed to ${contract.address}`)
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+  console.error(error)
+  process.exitCode = 1
+})
+
+// Contract deployed on Goerli test network at address:
+// 0x0693157bE694f1Acb330022B7707Ce5eedc1F4Ee
+// Contract verified on Etherscan
